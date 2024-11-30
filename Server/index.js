d8c1a9a9eb3430c68327ffb72d0e514b2050a12d
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-30 16:00:29 +0530

    Simple from add on client side

--- Server/index.js
+++ Server/index.js
@@ -20,6 +20,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    fetch();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
---
+++ simple-CRUD-client/src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+function App() {
+  const handleForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const users = { name, email };
+    console.log(users);
+  };
+  return (
+    <>
+      <h1>Simple CRUD (MongoDB)</h1>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-5">
+        <form onSubmit={handleForm} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
